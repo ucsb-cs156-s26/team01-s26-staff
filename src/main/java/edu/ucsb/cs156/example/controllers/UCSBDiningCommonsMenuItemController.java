de7877647892cfaaa486286c33f70cd64bcf04f6
Daniel Jensen <djensen2@outlook.com>
2026-04-16 20:09:54 -0700

    dj - added get endpoint and appropriate tests

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBDiningCommonsMenuItemController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBDiningCommonsMenuItemController.java
@@ -1,8 +1,11 @@
 package edu.ucsb.cs156.example.controllers;
 
 import edu.ucsb.cs156.example.entities.DiningCommonsMenuItem;
+import edu.ucsb.cs156.example.errors.EntityNotFoundException;
 import edu.ucsb.cs156.example.repositories.DiningCommonsMenuItemRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "UCSBDiningCommonsMenuItem")
 @RestController
 @RequestMapping("/api/ucsbdiningcommonsmenuitem")
-public class UCSBDiningCommonsMenuItemController {
+public class UCSBDiningCommonsMenuItemController extends ApiController {
 
   private final DiningCommonsMenuItemRepository diningCommonsMenuItemRepository;
 
@@ -55,5 +58,22 @@ public class UCSBDiningCommonsMenuItemController {
   @PreAuthorize("hasRole('ROLE_USER')")
   public Iterable<DiningCommonsMenuItem> allMenuItems() {
     return diningCommonsMenuItemRepository.findAll();
+  }
+
+  /**
+   * Get a single Menu Item by id
+   *
+   * @param id the id of the Menu Item
+   * @return a {@link DiningCommonsMenuItem}
+   */
+  @Operation(summary = "Get a single Menu Item")
+  @PreAuthorize("hasRole('ROLE_USER')")
+  @GetMapping("")
+  public DiningCommonsMenuItem getById(@Parameter(name = "id") @RequestParam Long id) {
+    DiningCommonsMenuItem menuItem =
+        diningCommonsMenuItemRepository
+            .findById(id)
+            .orElseThrow(() -> new EntityNotFoundException(DiningCommonsMenuItem.class, id));
+    return menuItem;
   }
 }
